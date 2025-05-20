9b8ffcc4c1f3f52026bd9ef02c0708abba740bc9
Jose Vitor <josevitorquadrado@gmail.com>
2025-05-20 12:21:38 -0300

    Add to Chapter11 PointerReceiver

---
+++ Chapter11/PointerReceiver/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type car struct {
+	color string
+}
+
+func (c *car) setColor(color string) {
+	c.color = color
+}
+
+func main() {
+	c := car{
+		color: "white",
+	}
+	c.setColor("blue")
+	fmt.Println(c.color)
+}
