a1075b0c447ee4269747320be6c6f8b3b5a827e4
Sherb <josevitorquadrado@gmail.com>
2025-05-27 22:06:30 -0300

    Add Importing code from another folder in HelloGo on Chapter12

--- Chapter12/HelloGo/go.mod
+++ Chapter12/HelloGo/go.mod
@@ -1,3 +1,9 @@
 module github.com/wagslane/hellogo
 
 go 1.24.3
+
+replace github.com/wagslane/mystrings v0.0.0 => ../MyStrings
+
+require (
+    github.com/wagslane/mystrings v0.0.0
+)
--- Chapter12/HelloGo/main.go
+++ Chapter12/HelloGo/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/wagslane/mystrings"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println(
+		mystrings.Reverse("Hello World!"),
+	)
 }
